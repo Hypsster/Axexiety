5134ef1a404b3c55589a313c42b6c8597840a8f1
Hypsster <me@loncosky.com>
2025-10-28 19:51:23 -0400

    changed message again

--- Axexiety/ContentView.swift
+++ Axexiety/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, baby!")
+            Text("I love you baby!")
         }
         .padding()
     }
