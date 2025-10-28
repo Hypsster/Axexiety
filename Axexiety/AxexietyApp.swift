b264f41811a6d1040d1f5056e8c5a72c7f70041c
Hypsster <me@loncosky.com>
2025-10-28 19:22:24 -0400

    Initial Commit

---
+++ Axexiety/AxexietyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxexietyApp.swift
+//  Axexiety
+//
+//  Created by Dale on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AxexietyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
