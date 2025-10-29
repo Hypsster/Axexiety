27f1cd20d1bc32ba0d2d502e26ce8f884288e1fa
madhumita1003 <email2smadhu@gmail.com>
2025-10-28 20:02:34 -0400

    testing my commit

--- Axexiety/ContentView.swift
+++ Axexiety/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("I love you baby!")
+            Text("testing by mad***")
         }
         .padding()
     }
